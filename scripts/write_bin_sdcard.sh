2669207b27637ad6c09856e775e2ca9e786823fa
eupn <eupn@protonmail.com>
2023-02-22 15:52:19 +0400

    Build release for sdcard

--- scripts/write_bin_sdcard.sh
+++ scripts/write_bin_sdcard.sh
@@ -5,5 +5,5 @@ set -e
 # Configure to your sdcard path
 export VOLUME="/Volumes/NO NAME"
 
-cargo build
-arm-none-eabi-objcopy -I elf32-littlearm -O binary ../target/armv7a-none-eabi/debug/atsama5d27 "$VOLUME/app.bin"
+cargo build --release
+arm-none-eabi-objcopy -I elf32-littlearm -O binary ../target/armv7a-none-eabi/release/atsama5d27 "$VOLUME/app.bin"
