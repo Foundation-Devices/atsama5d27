b95b961415b8ad594a8030fc501373a61c80f2f0
ennmichael <enntheprogrammer@gmail.com>
2023-05-23 21:28:39 +0200

    aes module

---
+++ utralib/src/generated.rs
@@ -0,0 +1,32 @@
+// Feature flags are checked inside build.rs for sanity, so we
+// can use them here assuming all targets are mutually exclusive.
+
+#[cfg(feature="precursor-perflib")]
+mod precursor_perf;
+#[cfg(feature="precursor-perflib")]
+pub use precursor_perf::*;
+
+#[cfg(feature="precursor-dvt")]
+mod precursor_dvt;
+#[cfg(feature="precursor-dvt")]
+pub use precursor_dvt::*;
+
+#[cfg(feature="renode")]
+mod renode;
+#[cfg(feature="renode")]
+pub use renode::*;
+
+#[cfg(feature="precursor-pvt")]
+mod precursor_pvt;
+#[cfg(feature="precursor-pvt")]
+pub use precursor_pvt::*;
+
+#[cfg(feature = "atsama5d27")]
+mod atsama5d27;
+#[cfg(feature = "atsama5d27")]
+pub use atsama5d27::*;
+
+// Hosted mode includes nothing, as it relies on the abstract host
+// architecture for I/O; so this file is empty when the "hosted"
+// configuration is selected and there are no corresponding "hosted"
+// mode feature flags.
