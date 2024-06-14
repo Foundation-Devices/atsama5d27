fbe3778e31fc442a8bb9f3f6e2f66e7d24f887f6
Alex Badics <admin@stickman.hu>
2024-06-14 14:49:00 +0200

    scripts: add gdb commands to reboot in SAM-BA mode

---
+++ scripts/reboot-in-samba-mode.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+arm-none-eabi-gdb -q <<SCRIPT
+target remote :3334
+monitor reset
+set  *((unsigned*)0xF8045400) = 0xfff
+set  *((unsigned*)0xF8048054) = 0x66830004
+monitor reset
+SCRIPT
