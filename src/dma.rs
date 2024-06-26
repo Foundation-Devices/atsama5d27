e74e28d81e7ad91b3ea6fd9e5807171f7777416b
eupn <eupn@protonmail.com>
2024-06-26 20:17:44 +0400

    dma: implement peripheral-to-memory DMA driver

--- src/dma.rs
+++ src/dma.rs
@@ -1,71 +1,218 @@
-use utralib::{utra::xdmac0, CSR};
+// SPDX-FileCopyrightText: 2024 Foundation Devices, Inc. <hello@foundationdevices.com>
+// SPDX-License-Identifier: MIT OR Apache-2.0
 
-// TODO I was not able to get the DMA functions to work. I'm going to leave them
-// as-is for now and tackle them again later.
+use utralib::{
+    utra::{xdmac0::*},
+    CSR,
+    HW_XDMAC0_BASE,
+    HW_XDMAC1_BASE,
+};
+
+pub struct Xdmac {
+    base_addr: u32,
+}
+
+impl Xdmac {
+    pub fn xdmac0() -> Xdmac {
+        Xdmac {
+            base_addr: HW_XDMAC0_BASE as u32,
+        }
+    }
+
+    pub fn xdmac1() -> Xdmac {
+        Xdmac {
+            base_addr: HW_XDMAC1_BASE as u32,
+        }
+    }
+
+    pub fn with_alt_base_addr(addr: usize) -> Xdmac {
+        Xdmac {
+            base_addr: addr as u32,
+        }
+    }
+
+    pub fn channel(&self, ch: DmaChannel) -> XdmacChannel {
+        XdmacChannel {
+            xdmac_base_addr: self.base_addr,
+            channel: ch,
+        }
+    }
+
+    pub fn gis(&self) -> u32 {
+        let dma = CSR::new(self.base_addr as *mut u32);
+        dma.r(XDMAC_GIS)
+    }
+
+    pub fn gim(&self) -> u32 {
+        let dma = CSR::new(self.base_addr as *mut u32);
+        dma.r(XDMAC_GIM)
+    }
+}
+
+pub struct XdmacChannel {
+    xdmac_base_addr: u32,
+    channel: DmaChannel,
+}
+
+impl XdmacChannel {
+    /// Sets up a peripheral-to-memory DMA transfer.
+    pub fn configure_peripheral_to_memory(&self, peripheral: Peripheral) {
+        let mut dma = CSR::new(self.xdmac_base_addr as *mut u32);
+
+        let cc_reg = match self.channel {
+            DmaChannel::Channel0 => XDMAC_CC0,
+            DmaChannel::Channel1 => XDMAC_CC1,
+        };
+
+        let cc = dma.ms(XDMAC_CC0_TYPE, 1)
+            | dma.ms(XDMAC_CC0_PERID, peripheral.id)
+            | dma.ms(XDMAC_CC0_DSYNC, 0) // PER2MEM
+            | dma.ms(XDMAC_CC0_PROT, 0)
+            | dma.ms(XDMAC_CC0_SWREQ, 0)
+            | dma.ms(XDMAC_CC0_DAM, 1)
+            | dma.ms(XDMAC_CC0_SAM, 0)
+            | dma.ms(XDMAC_CC0_SIF, 1)
+            | dma.ms(XDMAC_CC0_DIF, 0)
+            | dma.ms(XDMAC_CC0_DWIDTH, 0)
+            | dma.ms(XDMAC_CC0_CSIZE, 0)
+            | dma.ms(XDMAC_CC0_MBSIZE, 0);
+
+        dma.wo(cc_reg, cc);
+    }
+
+    /// Starts the DMA transfer.
+    ///
+    /// The DMA transfer must be configured beforehand by calling one of the following
+    /// methods:
+    /// - [`configure_peripheral_to_memory`](configure_peripheral_to_memory)
+    /// - TODO
+    pub fn execute_peripheral_transfer<const N: usize>(
+        &self,
+        peripheral: Peripheral,
+        dst: &mut Buffer<N>,
+        block_size: u32,
+    ) -> Result<(), &'static str> {
+        if N % 4 != 0 {
+            return Err("Destination buffer length is not a multiple of 4 bytes.");
+        }
+
+        let mut dma = CSR::new(self.xdmac_base_addr as *mut u32);
+        let (cis_reg, sa, da, ublen, en) = match self.channel {
+            DmaChannel::Channel0 => (
+                XDMAC_CIS0,
+                XDMAC_CSA0_SA,
+                XDMAC_CDA0_DA,
+                XDMAC_CUBC0_UBLEN,
+                XDMAC_GE_EN0,
+            ),
+            DmaChannel::Channel1 => (
+                XDMAC_CIS1,
+                XDMAC_CSA1_SA,
+                XDMAC_CDA1_DA,
+                XDMAC_CUBC1_UBLEN,
+                XDMAC_GE_EN1,
+            ),
+        };
+
+        // Clear the channel status
+        dma.r(cis_reg);
+
+        // Configure the transfer parameters
+        dma.wfo(sa, peripheral.address);
+        dma.wfo(da, dst.0.as_ptr() as u32);
+        dma.wfo(ublen, block_size);
+
+        // Make sure all memory transfers are completed before enabling the DMA
+        armv7::asm::dmb();
+
+        // Start the transfer
+        dma.wfo(en, 1);
+
+        Ok(())
+    }
+
+    pub fn interrupt_status(&self) -> u32 {
+        let dma = CSR::new(self.xdmac_base_addr as *mut u32);
+
+        match self.channel {
+            DmaChannel::Channel0 => dma.r(XDMAC_CIS0),
+            DmaChannel::Channel1 => dma.r(XDMAC_CIS1),
+        }
+    }
+
+    pub fn is_transfer_complete(&self) -> bool {
+        let cis = self.interrupt_status();
+
+        // Check if BIS=1
+        cis & 1 != 0
+    }
+
+    pub fn suspend(&self) {
+        let mut dma = CSR::new(self.xdmac_base_addr as *mut u32);
+        let ch_bit = self.channel as u32;
+
+        dma.wo(XDMAC_GRWS, 1 << ch_bit);
+    }
+
+    pub fn enable(&self) {
+        let mut dma = CSR::new(self.xdmac_base_addr as *mut u32);
+        let ch_bit = self.channel as u32;
+
+        dma.wo(XDMAC_GE, 1 << ch_bit);
+    }
+
+    pub fn disable(&self) {
+        let mut dma = CSR::new(self.xdmac_base_addr as *mut u32);
+        let ch_bit = self.channel as u32;
+
+        dma.wo(XDMAC_GD, 1 << ch_bit);
+    }
+
+    pub fn software_flush(&self) {
+        let mut dma = CSR::new(self.xdmac_base_addr as *mut u32);
+        let ch_bit = self.channel as u32;
+
+        dma.wo(XDMAC_GSWF, 1 << ch_bit);
+    }
+
+    pub fn set_interrupt(&self, enable: bool) {
+        let mut dma = CSR::new(self.xdmac_base_addr as *mut u32);
+        let ch_bit = self.channel as u32;
+
+        if enable {
+            dma.wo(XDMAC_GIE, 1 << ch_bit);
+        } else {
+            dma.wo(XDMAC_GID, 1 << ch_bit);
+        }
+    }
+
+    pub fn set_bi_interrupt(&self, enable: bool) {
+        let mut dma = CSR::new(self.xdmac_base_addr as *mut u32);
+
+        let (cie_reg, cid_reg) = match self.channel {
+            DmaChannel::Channel0 => (XDMAC_CIE0, XDMAC_CID0),
+            DmaChannel::Channel1 => (XDMAC_CIE1, XDMAC_CID1),
+        };
+
+        if enable {
+            dma.wo(cie_reg, 1 << 0); // BI
+        } else {
+            dma.wo(cid_reg, 1 << 0); // BI
+        }
+    }
+}
+
+#[derive(Debug, Copy, Clone)]
+pub enum DmaChannel {
+    Channel0 = 0,
+    Channel1 = 1,
+}
 
 /// Every memory buffer used for DMA must be aligned to 4 bytes.
 #[derive(Debug, Clone, Copy)]
 #[repr(C)]
 #[repr(align(4))]
 pub struct Buffer<const N: usize>(pub [u8; N]);
-
-/// Execute a memory-to-memory DMA transfer using channel 0.
-pub fn memory_to_memory<const NSRC: usize, const NDST: usize>(
-    src: &Buffer<NSRC>,
-    dst: &mut Buffer<NDST>,
-) -> Result<(), &'static str> {
-    let mut dma = CSR::new(utralib::HW_XDMAC0_BASE as *mut u32);
-
-    if NDST < NSRC {
-        return Err("Destination buffer is too small.");
-    }
-
-    if NSRC % 4 != 0 {
-        return Err("Source buffer length is not a multiple of 4 bytes.");
-    }
-
-    // Clear the pending Interrupt Status bits by reading the selected XDMAC
-    // Channel x Interrupt Status Register.
-    dma.r(xdmac0::XDMAC_CIS0);
-
-    // Write the XDMAC Source Address Register.
-    dma.wo(xdmac0::XDMAC_CSA0, src.0.as_ptr() as u32);
-
-    // Write the XDMAC Destination Address Register.
-    dma.wo(xdmac0::XDMAC_CDA0, dst.0.as_ptr() as u32);
-
-    // Set the UBLEN to the size of data.
-    dma.wfo(xdmac0::XDMAC_CUBC0_UBLEN, NSRC as u32);
-
-    // Clear the TYPE bit for a memory-to-memory transfer.
-    dma.wfo(xdmac0::XDMAC_CC0_TYPE, 0);
-
-    // Set the memory burst size to one byte.
-    dma.wfo(xdmac0::XDMAC_CC0_MBSIZE, 0);
-
-    // Set the source memory addressing mode to increment.
-    dma.wfo(xdmac0::XDMAC_CC0_SAM, 1);
-
-    // Set the destination memory addressing to increment.
-    dma.wfo(xdmac0::XDMAC_CC0_DAM, 1);
-
-    // Use a secured channel.
-    dma.wfo(xdmac0::XDMAC_CC0_PROT, 0);
-
-    // Data width is 1 byte.
-    dma.wfo(xdmac0::XDMAC_CC0_DWIDTH, 0);
-
-    // Set the peripheral ID for a memory-to-memory transfer.
-    dma.wfo(xdmac0::XDMAC_CC0_PERID, 127);
-
-    // Enable the DMA channel.
-    dma.wfo(xdmac0::XDMAC_GE_EN0, 1);
-
-    // Wait until the transfer is done.
-    while dma.rf(xdmac0::XDMAC_GS_ST0) == 1 {}
-
-    Ok(())
-}
 
 #[derive(Debug, Clone, Copy)]
 pub struct Peripheral {
@@ -75,119 +222,4 @@ pub struct Peripheral {
     pub chunk_size: u32,
     /// The address of the input register for this peripheral.
     pub address: u32,
-}
-
-/// Execute a memory-to-peripheral DMA transfer using channel 0.
-pub fn memory_to_peripheral<const N: usize>(
-    src: &Buffer<N>,
-    peripheral: Peripheral,
-) -> Result<(), &'static str> {
-    let mut dma = CSR::new(utralib::HW_XDMAC0_BASE as *mut u32);
-
-    if N % 4 != 0 {
-        return Err("Source buffer length is not a multiple of 4 bytes.");
-    }
-
-    // Clear the pending Interrupt Status bits by reading the selected XDMAC
-    // Channel x Interrupt Status Register.
-    dma.r(xdmac0::XDMAC_CIS0);
-
-    // Write the XDMAC Source Address Register.
-    dma.wo(xdmac0::XDMAC_CSA0, src.0.as_ptr() as u32);
-
-    // Write the XDMAC Destination Address Register.
-    dma.wo(xdmac0::XDMAC_CDA0, peripheral.address);
-
-    // Set the TYPE bit for a peripheral transfer.
-    dma.wfo(xdmac0::XDMAC_CC0_TYPE, 1);
-
-    // Data width is one word (32 bits).
-    dma.wfo(xdmac0::XDMAC_CC0_DWIDTH, 2);
-
-    // Set the chunk size for the peripheral.
-    dma.wfo(xdmac0::XDMAC_CC0_CSIZE, peripheral.chunk_size);
-
-    // MBSIZE does not matter for a memory-to-peripheral transfer.
-    dma.wfo(xdmac0::XDMAC_CC0_MBSIZE, 0);
-
-    // Set the UBLEN to the size of data. Divided by four because the DWIDTH is 4
-    // bytes.
-    dma.wfo(xdmac0::XDMAC_CUBC0_UBLEN, N as u32 / 4);
-
-    // Set the source memory addressing mode to increment.
-    dma.wfo(xdmac0::XDMAC_CC0_SAM, 1);
-
-    // Set the destination memory addressing mode to not change the address.
-    dma.wfo(xdmac0::XDMAC_CC0_DAM, 0);
-
-    // Memory-to-peripheral transfer.
-    dma.wfo(xdmac0::XDMAC_CC0_DSYNC, 1);
-
-    // Set the peripheral ID.
-    dma.wfo(xdmac0::XDMAC_CC0_PERID, peripheral.id);
-
-    Ok(())
-}
-
-/// Set up a peripheral-to-memory DMA transfer using channel 1.
-pub fn peripheral_to_memory<const N: usize>(
-    peripheral: Peripheral,
-    dst: &mut Buffer<N>,
-) -> Result<(), &'static str> {
-    let mut dma = CSR::new(utralib::HW_XDMAC0_BASE as *mut u32);
-
-    if N % 4 != 0 {
-        return Err("Destination buffer length is not a multiple of 4 bytes.");
-    }
-
-    // Clear the pending Interrupt Status bits by reading the selected XDMAC
-    // Channel x Interrupt Status Register.
-    dma.r(xdmac0::XDMAC_CIS1);
-
-    // Write the XDMAC Source Address Register.
-    dma.wo(xdmac0::XDMAC_CSA1, peripheral.address);
-
-    // Write the XDMAC Destination Address Register.
-    dma.wo(xdmac0::XDMAC_CDA1, dst.0.as_ptr() as u32);
-
-    // Data width is one word (32 bits).
-    dma.wfo(xdmac0::XDMAC_CC1_DWIDTH, 2);
-
-    // Set the chunk size for the peripheral.
-    dma.wfo(xdmac0::XDMAC_CC1_CSIZE, peripheral.chunk_size);
-
-    // Use one chunk per memory burst.
-    dma.wfo(xdmac0::XDMAC_CC1_MBSIZE, 0);
-
-    dma.wfo(xdmac0::XDMAC_CUBC1_UBLEN, N as u32 / 4);
-
-    // Set the TYPE bit for a peripheral transfer.
-    dma.wfo(xdmac0::XDMAC_CC1_TYPE, 1);
-
-    // Set the source memory addressing mode to not change the address.
-    dma.wfo(xdmac0::XDMAC_CC1_SAM, 0);
-
-    // Set the destination memory addressing mode to increment.
-    dma.wfo(xdmac0::XDMAC_CC1_DAM, 1);
-
-    // Peripheral-to-memory transfer.
-    dma.wfo(xdmac0::XDMAC_CC1_DSYNC, 0);
-
-    // Set the peripheral ID.
-    dma.wfo(xdmac0::XDMAC_CC1_PERID, peripheral.id);
-
-    Ok(())
-}
-
-/// Execute the peripheral transfer. This should be called after
-/// [`memory_to_peripheral`] and [`peripheral_to_memory`] have been called.
-pub fn execute_peripheral_transfer() {
-    let mut dma = CSR::new(utralib::HW_XDMAC0_BASE as *mut u32);
-
-    // Enable input and output channels to start the transfer.
-    dma.wfo(xdmac0::XDMAC_GE_EN0, 1);
-    dma.wfo(xdmac0::XDMAC_GE_EN1, 1);
-
-    // Wait until the transfer is done.
-    while dma.rf(xdmac0::XDMAC_GS_ST0) == 1 || dma.rf(xdmac0::XDMAC_GS_ST1) == 1 {}
 }
