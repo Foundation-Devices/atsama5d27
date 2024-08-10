9c0ba3455d17ac84b230a84b47addd10b32cce31
Alex Badics <admin@stickman.hu>
2024-08-10 17:30:04 +0200

    scripts/reboot: halt before memory write

--- scripts/reboot-in-samba-mode.sh
+++ scripts/reboot-in-samba-mode.sh
@@ -5,6 +5,7 @@ set -euo pipefail
 arm-none-eabi-gdb -q <<SCRIPT
 target remote :3334
 monitor reset
+monitor halt
 set  *((unsigned*)0xF8045400) = 0xfff
 set  *((unsigned*)0xF8048054) = 0x66830004
 monitor reset
